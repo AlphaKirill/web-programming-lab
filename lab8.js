a965721be4a6c6948c2cc6bea9cc31adb71343b6
Белкин Кирилл <kirya-belkin@list.ru>
2023-06-03 01:03:57 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -45,3 +45,12 @@ function clearDate() {
     inputDate.value = "";
     showDays.innerHTML = "";
 }
+
+function showTime() {
+
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+
+}
+setInterval(showTime, 1000);
